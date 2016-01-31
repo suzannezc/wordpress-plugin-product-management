c18d305f203af245f530292c23b040ae92a9d711
James Schumann <jschumann@gmail.com>
2016-01-31 16:43:52 -0500

    Add boilerplate meta box to project edit

--- project_post_type.php
+++ project_post_type.php
@@ -52,3 +52,89 @@ function wrdsb_project_post_type() {
 
 }
 add_action( 'init', 'wrdsb_project_post_type', 0 );
+
+/**
+ * Adds a box to the main column on the Project edit screens.
+ */
+function wrdsb_project_add_meta_box() {
+	add_meta_box(
+		'wrdsb_project_sectionid',
+		__( 'My Post Section Title', 'wrdsb_projects' ),
+		'wrdsb_project_meta_box_callback',
+		'wrdsb_project',
+		'advanced',
+		'high'
+	);
+}
+add_action( 'add_meta_boxes', 'wrdsb_project_add_meta_box' );
+
+// Move all "advanced" metaboxes above the default editor
+add_action('edit_form_after_title', function() {
+    global $post, $wp_meta_boxes;
+    do_meta_boxes(get_current_screen(), 'advanced', $post);
+    unset($wp_meta_boxes[get_post_type($post)]['advanced']);
+});
+
+/**
+ * Prints the box content.
+ * 
+ * @param WP_Post $post The object for the current post/page.
+ */
+function wrdsb_project_meta_box_callback( $post ) {
+	// Add a nonce field so we can check for it later.
+	wp_nonce_field( 'wrdsb_project_save_meta_box_data', 'wrdsb_project_meta_box_nonce' );
+	/*
+	 * Use get_post_meta() to retrieve an existing value
+	 * from the database and use the value for the form.
+	 */
+	$value = get_post_meta( $post->ID, '_my_meta_value_key', true );
+	echo '<label for="wrdsb_project_new_field">';
+	_e( 'Description for this field', 'wrdsb_projects' );
+	echo '</label> ';
+	echo '<input type="text" id="wrdsb_project_new_field" name="wrdsb_project_new_field" value="' . esc_attr( $value ) . '" size="25" />';
+}
+
+/**
+ * When the post is saved, saves our custom data.
+ *
+ * @param int $post_id The ID of the post being saved.
+ */
+function wrdsb_project_save_meta_box_data( $post_id ) {
+	/*
+	 * We need to verify this came from our screen and with proper authorization,
+	 * because the save_post action can be triggered at other times.
+	 */
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['wrdsb_project_meta_box_nonce'] ) ) {
+		return;
+	}
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST['wrdsb_project_meta_box_nonce'], 'wrdsb_project_save_meta_box_data' ) ) {
+		return;
+	}
+	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+	} else {
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+	}
+	/* OK, it's safe for us to save the data now. */
+	
+	// Make sure that it is set.
+	if ( ! isset( $_POST['wrdsb_project_new_field'] ) ) {
+		return;
+	}
+	// Sanitize user input.
+	$my_data = sanitize_text_field( $_POST['wrdsb_project_new_field'] );
+	// Update the meta field in the database.
+	update_post_meta( $post_id, '_my_meta_value_key', $my_data );
+}
+add_action( 'save_post', 'wrdsb_project_save_meta_box_data' );
