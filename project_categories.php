e1efa0d926e628177b9abea69e30f3cf956a71f2
James Schumann <jschumann@gmail.com>
2016-01-31 15:26:30 -0500

    Create project_categories.php

---
+++ project_categories.php
@@ -0,0 +1,39 @@
+<?php
+// Register Custom Taxonomy
+function wrdsb_project_categories_custom_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Project Categories', 'Taxonomy General Name', 'wrdsb_projects' ),
+		'singular_name'              => _x( 'Project Category', 'Taxonomy Singular Name', 'wrdsb_projects' ),
+		'menu_name'                  => __( 'Project Categories', 'wrdsb_projects' ),
+		'all_items'                  => __( 'All Project Categories', 'wrdsb_projects' ),
+		'parent_item'                => __( 'Parent Project Category', 'wrdsb_projects' ),
+		'parent_item_colon'          => __( 'Parent Project Category:', 'wrdsb_projects' ),
+		'new_item_name'              => __( 'New Project Category Name', 'wrdsb_projects' ),
+		'add_new_item'               => __( 'Add New Project Category', 'wrdsb_projects' ),
+		'edit_item'                  => __( 'Edit Project Category', 'wrdsb_projects' ),
+		'update_item'                => __( 'Update Project Category', 'wrdsb_projects' ),
+		'view_item'                  => __( 'View Project Category', 'wrdsb_projects' ),
+		'separate_items_with_commas' => __( 'Separate categories with commas', 'wrdsb_projects' ),
+		'add_or_remove_items'        => __( 'Add or remove categories', 'wrdsb_projects' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'wrdsb_projects' ),
+		'popular_items'              => __( 'Popular Project Categories', 'wrdsb_projects' ),
+		'search_items'               => __( 'Search Project Categories', 'wrdsb_projects' ),
+		'not_found'                  => __( 'Not Found', 'wrdsb_projects' ),
+		'no_terms'                   => __( 'No project categories', 'wrdsb_projects' ),
+		'items_list'                 => __( 'Project Categories list', 'wrdsb_projects' ),
+		'items_list_navigation'      => __( 'Project categories list navigation', 'wrdsb_projects' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'wrdsb_project_categories', array( 'wrdsb_project' ), $args );
+
+}
+add_action( 'init', 'wrdsb_project_categories_custom_taxonomy', 0 );
